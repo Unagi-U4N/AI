00cae88e9f9976488894aed124b2b64de8016a94
Unagi-U4N <icivanunagi@gmail.com>
2024-03-12 23:30:52 +0800

    Minor changes

--- Intro/degrees/degrees.py
+++ Intro/degrees/degrees.py
@@ -53,11 +53,9 @@ def load_data(directory):
 
 
 def main():
-    # if len(sys.argv) > 2:
-    #     sys.exit("Usage: python degrees.py [directory]")
-    # directory = sys.argv[1] if len(sys.argv) == 2 else "large"
-
-    directory = r"c:\Users\ICiva\coding project\AI\Intro\degrees\small"
+    if len(sys.argv) > 2:
+        sys.exit("Usage: python degrees.py [directory]")
+    directory = sys.argv[1] if len(sys.argv) == 2 else "large"
 
     # Load data from files into memory
     print("Loading data...")
